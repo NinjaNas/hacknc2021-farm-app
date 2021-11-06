878e7df200b70f459f27ef1b9519421cf1aee4e9
Scott Hoover <sc0tt@email.unc.edu>
2021-11-06 13:28:48 -0400

    Hackathon

--- src/main/java/com/hacknc/App.java
+++ src/main/java/com/hacknc/App.java
@@ -1,13 +1,24 @@
 package com.hacknc;
 
+import javax.swing.JFrame;
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JFrame myFrame = new JFrame("Game!");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setTitle("Big Farm Tycoon!");
+        myFrame.setResizable(false);
+        myFrame.setLocationRelativeTo(null);
+        myFrame.setSize(500, 500);
+        myFrame.setVisible(true);
+
     }
+
+
+
 }
