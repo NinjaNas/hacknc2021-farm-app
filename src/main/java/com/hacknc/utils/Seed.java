ac4708d0347f9340652abb6f1866bcdc74881682
Isidro Casiano <personal@isidrocasiano.com>
2021-11-06 13:37:25 -0400

    feat(Seed): constructor, plantSeed, and setCurrentSeed

--- src/main/java/com/hacknc/utils/Seed.java
+++ src/main/java/com/hacknc/utils/Seed.java
@@ -1,13 +1,41 @@
 package com.hacknc.utils;
 
+enum Type {
+    WHEAT,
+    CORN,
+    BEANS,
+    CARROT,
+    BEET,
+    TOMOTO;
+}
+
 public final class Seed {
-    public enum Type {
-        WHEAT,
-        CORN,
-        BEANS,
-        CARROT,
-        BEET,
-        TOMOTO;
+    static Type seed;
+
+    public Seed(Type seed) {
+        Seed.seed = seed;
+    }
+    public static void plantSeed() {
+        switch (seed) {
+            case WHEAT:
+                break;
+            case BEANS:
+                break;
+            case BEET:
+                break;
+            case CARROT:
+                break;
+            case CORN:
+                break;
+            case TOMOTO:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void setCurrentSeed(Type seed) {
+        Seed.seed = seed;
     }
 }
 
