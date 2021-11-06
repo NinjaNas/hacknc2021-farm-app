98f88e98b560af5e68630ca3e9cff0053d1d2a9f
Scott Hoover <sc0tt@email.unc.edu>
2021-11-06 17:11:35 -0400

    Work

--- src/main/java/com/hacknc/App.java
+++ src/main/java/com/hacknc/App.java
@@ -7,8 +7,12 @@ import javax.swing.JFrame;
  */
 public class App
 {
+
+
     public static void main( String[] args ) {
-        JFrame myFrame = new JFrame("Game!");
+
+        Jframe init = new Jframe(new JFrame());
+        JFrame myFrame = Jframe.create().getFrame();
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.setTitle("Big Farm Tycoon!");
         myFrame.setResizable(false);
@@ -16,14 +20,28 @@ public class App
         myFrame.setSize(500, 500);
         myFrame.setVisible(true);
         boolean runtheGame = true;
+        double elapsed_time = 0.0;
+
         while(runtheGame){
+            for(int i = 0; i < 10; i++){
+                System.out.printf(" \n");
+            }
             //update game
             //render game
             System.out.printf("%f\n", FPS.getDeltaTime());
+
             //recalculate delta time
             FPS.calcDeltaTime();
+            elapsed_time += FPS.getDeltaTime();
+            System.out.printf("%f\n", elapsed_time);
         }
 
+    }
+
+
+    public static JFrame create(){
+        JFrame t = new JFrame();
+        return t;
     }
 
 }
