15c8125fa5bcec3bcddcd5473555eedd0e89a3be
Khang Tran <ktra@email.unc.edu>
2021-11-06 18:52:19 -0400

    Test MainMenu

--- src/main/java/com/hacknc/App.java
+++ src/main/java/com/hacknc/App.java
@@ -1,51 +1,33 @@
 package com.hacknc;
 
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.BoxLayout;
-/**
- * Hello world!
- *
- */
-public class App
+import javax.swing.*;
+
+public class App extends JFrame
 {
-
-
     public static void main( String[] args ) {
-
-        Jframe init = new Jframe(new JFrame());
-        JFrame myFrame = Jframe.create().getFrame();
-
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame myFrame = Frame.create();
+        myFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         myFrame.setTitle("Big Farm Tycoon!");
         myFrame.setResizable(true);
         myFrame.setLocationRelativeTo(null);
         myFrame.setSize(200, 200);
         myFrame.setVisible(true);
-        boolean runtheGame = true;
+        MainMenu.create();
         double elapsed_time = 0.0;
+        while(true){
+            if(MainMenu.create().getStart()) {
+                for (int i = 0; i < 10; i++) {
+                    System.out.printf(" \n");
+                }
+                //update game
+                //render game
+                System.out.println(Long.toString(FPS.getDeltaTime()));
 
-        while(runtheGame){
-            for(int i = 0; i < 10; i++){
-                System.out.printf(" \n");
+                //recalculate delta time
+                FPS.calcDeltaTime();
+                elapsed_time += FPS.getDeltaTime();
+                System.out.printf("%f\n", elapsed_time);
             }
-            //update game
-            //render game
-            System.out.printf("%f\n", FPS.getDeltaTime());
-
-            //recalculate delta time
-            FPS.calcDeltaTime();
-            elapsed_time += FPS.getDeltaTime();
-            System.out.printf("%f\n", elapsed_time);
         }
     }
-
-
-    public static JFrame create(){
-        JFrame t = new JFrame();
-        return t;
-    }
-
 }
