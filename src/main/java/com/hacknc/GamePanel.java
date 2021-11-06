18d8510264adad66ef8e8d936d02510e9155a5d8
Nikolas <n.jauch@ymail.com>
2021-11-06 18:59:14 -0400

    Fixed Merge Issues

--- src/main/java/com/hacknc/GamePanel.java
+++ src/main/java/com/hacknc/GamePanel.java
@@ -4,10 +4,6 @@ package com.hacknc;
 
 import javax.swing.*;
 
-public class GamePanel extends JPanel {
-    JFrame menu_ = new JFrame("Main MainMenu");
-
-
 public interface GamePanel {
     //Returns the game view of the panel
     public JPanel getGamePanel();
